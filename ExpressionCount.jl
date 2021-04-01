5f5fe346f724da772bebaf70b88ca712645768a8
Moriah Echlin <moriah.echlin@gmail.com>
2021-04-01 18:24:29 +0300

    Analysis Modules

---
+++ ExpressionCount.jl
@@ -0,0 +1,27 @@
+module ExpressionCount
+
+using StatsBase,StatsPlots
+
+function expHist(SeqVals_17,SeqVals_27,SeqVals_Pd)
+    CountPerCell = sum.((SeqVals_17.!=0,SeqVals_27.!=0,SeqVals_Pd.!=0),dims=1); #number of nonzeros per cell barcode
+    Count_hist_17=fit(Histogram,CountPerCell[1][:],[0:500:7500;]); # histogram of the number of nonzeros per cell barcode
+    Count_hist_27=fit(Histogram,CountPerCell[2][:],[0:500:7500;]); # histogram of the number of nonzeros per cell barcode
+    Count_hist_Pd=fit(Histogram,CountPerCell[3][:],[0:500:7500;]); # histogram of the number of nonzeros per cell barcode
+    #Grouped (by donor) histogram of cells by the number of expressed genes
+    fig=groupedbar([Count_hist_17.weights Count_hist_27.weights Count_hist_Pd.weights],
+        xlim = (0,17),
+        ylim = (0,2*10^4),
+        xlabel = "Number of Reported Genes Expressed (1x10³)",
+        ylabel = "Cell Counts (1x10³)",
+        xticks = ([0:2:17;],string.([0:1:17/2;])),
+        yticks = ([0:5*10^3:2*10^4;],string.([0:5:20;])),
+        xtickfont=font(10),
+        ytickfont=font(10),
+        guidefont=font(12),
+        legendfont = font(10),
+        label = ["Donor 17" "Donor 27" "Peripheral/Transition Zones"],
+        title = "Henry et al. Single Cell Data",
+        framestyle=:box)
+    savefig(fig,"Henryetal_ExpressedGenesHist.png");
+end #function
+end #module
---
+++ RNA_HD.jl
@@ -0,0 +1,37 @@
+module RNA_HD
+
+using StatsBase,StatsPlots,Statistics
+# HD will create a Hamming distance vector of N randomly sampled pairs of cells, without replacement.
+function HD(SeqVals,N)
+    SeqVals .= SeqVals.>0 #Binarize SeqVals
+    G,C = size(SeqVals) #Number of genes and cells
+    Seq_HD = fill(0.0,N) #Initialized vector of Hamming Distance between cells
+    PairIJ = reshape(sample([1:C;],N*2,replace=false),N,2) # Cell pair indices
+    for i = 1:N
+        Seq_HD[i] = sum(xor.(SeqVals[:,PairIJ[i,1]],SeqVals[:,PairIJ[i,2]])) / G # Compare Cell 1 to Cell 2,
+    end #for
+    Seq_HD
+end #function
+
+function analysis(SeqVals,N,source)
+    SeqHD = HD(SeqVals,N) # get HD values
+    fig = histogram(SeqHD,  #generate histogram of values
+        bins = [0:0.01:1;],
+        xlim = (0,maximum(SeqHD)+0.02),
+        ylim = (0,N/2),
+        xlabel = "Hamming Distance Between $(N) Sampled Cell Pairs",
+        #ylabel = "",
+        #xticks = ([0:0:0;],string.([0:0:0;])),
+        #yticks = ([0:0:0;],string.([0:0:0;])),
+        xtickfont=font(10),
+        ytickfont=font(10),
+        guidefont=font(12),
+        legendfont = font(10),
+        legend = false,
+        #label = [""],
+        title = "Henry et al. Single Cell Data",
+        framestyle=:box)
+    savefig(fig,"Henryetal_CellHD_$(source)N$(N).png"); #savefig
+end #function
+
+end#module
